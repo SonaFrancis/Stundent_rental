88507f1354b612d27f61c6e789e7f527d5e9ab91
SonaFrancis <82178136+SonaFrancis@users.noreply.github.com>
2025-08-19 23:24:09 +0400

    Fix Expo SDK version and Metro configuration

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Ensure proper resolver configuration
+config.resolver.assetExts.push('sql');
+
 module.exports = config;
